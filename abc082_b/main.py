9b475975cdc504f32ce919b545846a4e6e070c6e
dekokun <dekokun@hatena.ne.jp>
2020-04-22 21:14:17 +0900

    abc082_b pass

--- abc082_b/main.py
+++ abc082_b/main.py
@@ -4,3 +4,23 @@ input = sys.stdin.readline
 
 def log(v):
     print(v, file=sys.stderr)
+
+
+s = sorted(input().strip())
+t = sorted(input().strip())
+t.reverse()
+
+for i in range(0, max(len(s), len(t))):
+    if len(s) <= i:
+        print("Yes")
+        exit(0)
+    if len(t) <= i:
+        print("No")
+        exit(0)
+    if s[i] > t[i]:
+        print("No")
+        exit(0)
+    if s[i] < t[i]:
+        print("Yes")
+        exit(0)
+print("No")
