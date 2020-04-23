adb66c16899fc21a09a99a320de44fc3e12a3042
dekokun <dekokun@hatena.ne.jp>
2020-04-23 21:40:34 +0900

    agc015_a pass

--- agc015_a/main.py
+++ agc015_a/main.py
@@ -4,3 +4,9 @@ input = sys.stdin.readline
 
 def log(v):
     print(v, file=sys.stderr)
+
+
+N, A, B = map(int, input().split())
+maxNum = A + B * (N - 1)
+minNum = A * (N - 1) + B
+print(max(maxNum - minNum + 1, 0))
