fb49365f4000a14a4a60f70ebddbd1bcf66dee6f
dekokun <dekokun@hatena.ne.jp>
2020-04-24 13:53:50 +0900

    abc008_2 pass

--- abc008_2/main.py
+++ abc008_2/main.py
@@ -4,3 +4,15 @@ input = sys.stdin.readline
 
 def log(v):
     print(v, file=sys.stderr)
+
+
+counts = dict()
+N = int(input())
+for _ in range(0, N):
+    name = input().strip()
+    if not name in counts:
+        counts[name] = 0
+    counts[name] += 1
+for v in sorted(counts, key=counts.get, reverse=True):
+    print(v)
+    exit()
