ba78f9a1e91cc147ecb09c067a4bcb99841be945
dekokun <dekokun@hatena.ne.jp>
2020-04-25 21:21:29 +0900

    test case 1 pass

--- codejam_2020_round1b_blindfolded_bullseye/main.py
+++ codejam_2020_round1b_blindfolded_bullseye/main.py
@@ -4,3 +4,27 @@ input = sys.stdin.readline
 
 def log(v):
     print(v, file=sys.stderr)
+
+
+def output(v):
+    print(v, flush=True)
+
+
+def solve():
+    for i in range(-5, 6):
+        for j in range(-5, 6):
+            throw = "{} {}".format(i, j)
+            log(throw)
+            output(throw)
+            out = input().strip()
+            if out == "CENTER":
+                log("PASS")
+                return
+            elif out == "WRONG":
+                log("WRONG")
+                return
+
+
+T, A, B = map(int, input().split())
+for t in range(1, T + 1):
+    solve()
