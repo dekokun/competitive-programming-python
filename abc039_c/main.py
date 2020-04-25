4f8a59219d484acb45c2b53ce067439e6fd234c3
dekokun <dekokun@hatena.ne.jp>
2020-04-25 20:25:17 +0900

    abc039_c pass

--- abc039_c/main.py
+++ abc039_c/main.py
@@ -4,3 +4,17 @@ input = sys.stdin.readline
 
 def log(v):
     print(v, file=sys.stderr)
+
+
+from_first = "WBWBWWBWBWBWWBWBWWBWBWBW"
+
+s = input()
+first_five = s[0:11]
+
+dictionary = {0: "Do", 2: "Re", 4: "Mi", 5: "Fa", 7: "So", 9: "La", 11: "Si"}
+
+for i in range(0, 12):
+    if from_first[i:i+11] == first_five:
+        print(dictionary[i])
+        exit(0)
+exit(1)
