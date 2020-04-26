8bd0d1fe8a2e05f3f90abc820a4b63dd4ea322ad
dekokun <dekokun@hatena.ne.jp>
2020-04-26 21:07:01 +0900

    abc164_b pass

--- abc164_b/main.py
+++ abc164_b/main.py
@@ -1,5 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
-    print(*args, file=sys.stderr)k
+    print(*args, file=sys.stderr)
+
+
+A, B, C, D = map(int, input().split())
+while True:
+    C -= B
+    if C <= 0:
+        print('Yes')
+        exit(0)
+    A -= D
+    if A <= 0:
+        print('No')
+        exit(0)
