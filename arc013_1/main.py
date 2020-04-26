657905612922f7d8bb262a6003989c99689fc869
dekokun <dekokun@hatena.ne.jp>
2020-04-26 09:31:01 +0900

    arc013_1 pass

--- arc013_1/main.py
+++ arc013_1/main.py
@@ -2,5 +2,12 @@ import sys
 input = sys.stdin.readline
 
 
-def log(v):
-    print(v, file=sys.stderr)
+def log(*args):
+    print(*args, file=sys.stderr)
+
+
+N, M, L = map(int, input().split())
+P, Q, R = map(int, input().split())
+print(max((N//P) * (M//Q) * (L//R), (N//Q) *
+          (M//R) * (L//P), (N//R) * (M//P) * (L//Q), (N//P) * (L//Q) * (M//R), (N//Q) *
+          (L//R) * (M//P), (N//R) * (L//P) * (M//Q)))
