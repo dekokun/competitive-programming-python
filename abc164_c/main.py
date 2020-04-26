83e62c9b8c7e954dba7249eb5871ad2b74aedff5
dekokun <dekokun@hatena.ne.jp>
2020-04-26 21:10:39 +0900

    abc164_c pass

--- abc164_c/main.py
+++ abc164_c/main.py
@@ -1,5 +1,14 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
-    print(*args, file=sys.stderr)k
+    print(*args, file=sys.stderr)
+
+
+all = set()
+
+N = int(input())
+for _ in range(0, N):
+    all.add(input())
+print(len(all))
