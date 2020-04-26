b3ed92f5ff9c8b249fe1868ee5d843882d50c316
dekokun <dekokun@hatena.ne.jp>
2020-04-26 21:03:32 +0900

    abc164_a pass

--- abc164_a/main.py
+++ abc164_a/main.py
@@ -1,5 +1,13 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
-    print(*args, file=sys.stderr)k
+    print(*args, file=sys.stderr)
+
+
+S, W = map(int, input().split())
+if S > W:
+    print("safe")
+else:
+    print("unsafe")
