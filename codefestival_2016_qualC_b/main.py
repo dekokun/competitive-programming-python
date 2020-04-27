b6419c945bd35aadecb1a01f61ce919c617fd763
dekokun <dekokun@hatena.ne.jp>
2020-04-27 10:17:44 +0900

    codefestival_2016_qualC_b pass

--- codefestival_2016_qualC_b/main.py
+++ codefestival_2016_qualC_b/main.py
@@ -1,5 +1,12 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
-    print(*args, file=sys.stderr)k
+    print(*args, file=sys.stderr)
+
+
+K, T = map(int, input().split())
+counts = map(int, input().split())
+counts = sorted(counts, reverse=True)
+print(max(0, counts[0] - (sum(counts) - counts[0]) - 1))
