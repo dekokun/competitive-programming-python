bea6e8c2d2d6bcddc0267012d7a30575eb382410
dekokun <dekokun@hatena.ne.jp>
2020-05-01 20:55:57 +0900

    arc091_a pass

--- arc091_a/main.py
+++ arc091_a/main.py
@@ -1,5 +1,23 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
+
+
+N, M = map(int, input().split())
+if N == 1 and M == 1:
+    print(1)
+    exit(0)
+# Nは1じゃない状態
+elif N == 1:
+    N, M = M, N
+if M == 1:
+    print(N - 2)
+    exit(0)
+
+corner = 4
+edge = (N - 2) * 2 + (M - 2) * 2
+
+print(N * M - corner - edge)
