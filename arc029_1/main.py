d98952ea4e9ec2c70416a92567fee33cdbb3c2a2
dekokun <dekokun@hatena.ne.jp>
2020-05-02 15:38:19 +0900

    arc029_1 pass

--- arc029_1/main.py
+++ arc029_1/main.py
@@ -1,5 +1,26 @@
+from itertools import combinations
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
+
+
+N = int(input())
+l = []
+for _ in range(0, N):
+    l.append(int(input()))
+ans = []
+
+
+def dfs(depth, a, b):
+    if depth == N:
+        ans.append(max(a, b))
+    else:
+        dfs(depth + 1, a + l[depth], b)
+        dfs(depth + 1, a, b + l[depth])
+
+
+dfs(0, 0, 0)
+print(min(ans))
