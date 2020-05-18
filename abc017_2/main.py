feea788c08c830b0cdb69d1ed2237765ebb54963
dekokun <dekokun@hatena.ne.jp>
2020-05-18 21:39:21 +0900

    abc017_2 pass

--- abc017_2/main.py
+++ abc017_2/main.py
@@ -1,5 +1,25 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
+
+
+s = input().strip()
+search = ["o", "k", "u"]
+ss = list(s)
+before_c = False
+for i in range(0, len(ss)):
+    if before_c and ss[i] == 'h':
+        before_c = False
+        continue
+    if ss[i] == 'c' and not before_c:
+        before_c = True
+        continue
+    else:
+        if ss[i] in search:
+            continue
+    print("NO")
+    exit(0)
+print("YES")
