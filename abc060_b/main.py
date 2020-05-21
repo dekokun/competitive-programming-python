2fbbb2f635a9c4ebd60c08a43b219f55975f7c66
dekokun <dekokun@hatena.ne.jp>
2020-05-21 20:51:30 +0900

    abc060_b pass

--- abc060_b/main.py
+++ abc060_b/main.py
@@ -1,5 +1,24 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
+
+
+def main():
+    A, B, C = map(int, input().strip().split())
+    rems = set()
+    for i in range(0, 101):
+        v = (A * i) % B
+        if v == C:
+            print("YES")
+            return
+        if v in rems:
+            print("NO")
+            return
+        rems.add(v)
+
+
+if __name__ == '__main__':
+    main()
