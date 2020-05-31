0bcfdb13ee32a50be02a5f1dc78ccbdd9c6a885a
dekokun <dekokun@hatena.ne.jp>
2020-05-31 14:52:46 +0900

    abc002_3 pass

--- abc002_3/main.py
+++ abc002_3/main.py
@@ -1,10 +1,20 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    (xa, xb, ya, yb, za, zb) = map(int, input().split())
+    ya -= xa
+    yb -= xb
+    za -= xa
+    zb -= xb
+    log(ya, yb, )
+    print(abs(ya * zb - yb * za) / 2)
+
 
 if __name__ == '__main__':
     main()
