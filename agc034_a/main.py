75314d359a32a1513ac88fc98b7522e7f4001bd9
dekokun <dekokun@hatena.ne.jp>
2020-06-01 22:00:12 +0900

    agc034_a pass

--- agc034_a/main.py
+++ agc034_a/main.py
@@ -1,10 +1,27 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    N, A, B, C, D = map(int, input().split())
+    log(N, A, B, C, D)
+    S = input()
+    if "##" in S[A-1:max(D, C)]:
+        print("No")
+        return
+    if C < D:
+        print("Yes")
+        return
+    log(S[B-2:D+1])
+    if "..." in S[B-2:D+1]:
+        print("Yes")
+        return
+    print("No")
+
 
 if __name__ == '__main__':
     main()
