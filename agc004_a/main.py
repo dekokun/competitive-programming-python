513e6e37d1fb083b2e799b1697b9070a4ccd08ff
dekokun <dekokun@hatena.ne.jp>
2020-06-04 08:28:43 +0900

    agc004_a pass

--- agc004_a/main.py
+++ agc004_a/main.py
@@ -1,10 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    l = list(map(int, input().split()))
+    l = sorted(l)
+    small = l[2] // 2
+    large = l[2] - small
+    print((large - small) * l[0] * l[1])
+
 
 if __name__ == '__main__':
     main()
