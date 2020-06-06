a6d5aa803305a2be7f9a54e2568969b58c87ac1a
dekokun <dekokun@hatena.ne.jp>
2020-06-06 19:55:19 +0900

    arc006_3 pass

--- arc006_3/main.py
+++ arc006_3/main.py
@@ -1,10 +1,29 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    n = int(input())
+    l = []
+    for _i in range(0, n):
+        weight = int(input())
+        max_index = len(l)
+        for i in range(len(l)):
+            v = l[i]
+            if v >= weight and max_index == len(l):
+                max_index = i
+            elif v >= weight and l[max_index] < weight:
+                max_index = i
+        if max_index == len(l):
+            l.append(weight)
+        else:
+            l[max_index] = weight
+    print(len(l))
+
 
 if __name__ == '__main__':
     main()
