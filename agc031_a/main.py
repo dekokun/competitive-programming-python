fe238259f60f2d3da68b757ee68d0023406b339c
dekokun <dekokun@hatena.ne.jp>
2020-06-09 20:59:32 +0900

    agc031_a pass

--- agc031_a/main.py
+++ agc031_a/main.py
@@ -1,10 +1,27 @@
 import sys
+import itertools as it
 input = sys.stdin.readline
+
 
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    n = int(input())
+    ss = list(input().strip())
+    m = {}
+    for c in ss:
+        if c not in m:
+            m[c] = 0
+        m[c] += 1
+    log(m)
+    ans = 1
+    for k in m:
+        ans *= m[k] + 1
+        ans %= 10**9 + 7
+    print(ans - 1)
+
 
 if __name__ == '__main__':
     main()
