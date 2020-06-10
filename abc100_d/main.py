1b062f9a36a3a8c3d8b2d370bb8840fadea5dc04
dekokun <dekokun@hatena.ne.jp>
2020-06-10 09:45:00 +0900

    abc100_d pass

--- abc100_d/main.py
+++ abc100_d/main.py
@@ -1,10 +1,27 @@
 import sys
+from itertools import product
 input = sys.stdin.readline
+
 
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    n, m = map(int, input().split())
+    l = [list(map(int, input().split())) for _ in range(n)]
+    ans = 0
+    for i in [-1, 1]:
+        for j in [-1, 1]:
+            for k in [-1, 1]:
+                l2 = []
+                for (x, y, z) in l:
+                    s = x * i + y * j + z * k
+                    l2.append(s)
+                l2 = sorted(l2, reverse=True)
+                ans = max(ans, sum(l2[:m]))
+    print(ans)
+
 
 if __name__ == '__main__':
     main()
