764eda35f56bf54281a5c0fd03d427770cf44e84
dekokun <dekokun@hatena.ne.jp>
2020-06-16 13:56:55 +0900

    abc041_c pass

--- abc041_c/main.py
+++ abc041_c/main.py
@@ -1,10 +1,20 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    n = int(input())
+    a = list(map(int, input().strip().split()))
+    b = []
+    for i in range(n):
+        b.append((a[i], i + 1))
+    for v in sorted(b, reverse=True):
+        print(v[1])
+
 
 if __name__ == '__main__':
     main()
