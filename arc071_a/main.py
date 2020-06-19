9f985b3010cb26b1ec4e3795be8e77cb6ce77bb8
dekokun <dekokun@hatena.ne.jp>
2020-06-19 20:30:25 +0900

    tmp commit

--- arc071_a/main.py
+++ arc071_a/main.py
@@ -1,10 +1,23 @@
 import sys
 input = sys.stdin.readline
 
+
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    n = int(input())
+    l = []
+    for _ in range(n):
+        l.append(sorted(input().strip()))
+    for c in range(ord('a'), ord('z') + 1):
+        log(chr(c))
+        # それぞれのリストにどの数がなんぼあるかを数える
+    for c in range(ord('a'), ord('z') + 1):
+        # それぞれの文字が最低何文字あるかを数える
+        # 連結する
+
 
 if __name__ == '__main__':
     main()
