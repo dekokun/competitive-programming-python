4e43d37e5e895592fa09996c322a7d11ab2ba5e6
dekokun <dekokun@hatena.ne.jp>
2020-06-23 08:38:48 +0900

    arc041_b pass

--- arc041_b/main.py
+++ arc041_b/main.py
@@ -7,8 +7,20 @@ def log(*args):
 
 
 def main():
+    n, m = map(int, input().strip().split())
+    board = [list(map(int, (input().rstrip()))) for _ in range(n)]
+    ans = [[0] * m for _ in range(n)]
+    for (i, row) in enumerate(board):
+        for (j, cell) in enumerate(row):
+            if cell != 0:
+                board[i][j] = 0
+                ans[i + 1][j] += cell
+                board[i + 1][j - 1] -= cell
+                board[i + 1][j + 1] -= cell
+                board[i + 2][j] -= cell
+    for row in ans:
+        print("".join(map(str, row)))
 
 
 if __name__ == '__main__':
     main()
-
