880c664d1d2e93d789934d66e8ab54ac3c3f1e4f
dekokun <dekokun@hatena.ne.jp>
2020-06-22 15:31:14 +0900

    abc089_c pass

--- abc089_c/main.py
+++ abc089_c/main.py
@@ -1,10 +1,24 @@
 import sys
+from itertools import combinations
 input = sys.stdin.readline
+
 
 def log(*args):
     print(*args, file=sys.stderr)
 
+
 def main():
+    n = int(input().rstrip())
+    m = {'M': 0, 'A': 0, 'R': 0, 'C': 0, 'H': 0}
+    for _ in range(n):
+        s = input().rstrip()
+        if s[0] in m:
+            m[s[0]] += 1
+    ans = 0
+    for c in combinations(m, 3):
+        ans += m[c[0]] * m[c[1]] * m[c[2]]
+    print(ans)
+
 
 if __name__ == '__main__':
     main()
