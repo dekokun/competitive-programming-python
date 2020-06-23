e45541a900647e0ff2403d4d12e5718057df6c29
dekokun <dekokun@hatena.ne.jp>
2020-06-24 07:33:10 +0900

    arc051_b pass

--- arc051_b/main.py
+++ arc051_b/main.py
@@ -7,8 +7,24 @@ def log(*args):
 
 
 def main():
+    k = int(input().rstrip())
+    a = 1
+    b = 1
+    for _ in range(k):
+        a, b = b + a, a
+    print("{} {}".format(a, b))
+
+
+def gcd(a, b):
+    return gcd_rec(a, b, 0)
+
+
+def gcd_rec(a, b, counter):
+    if b == 0:
+        return a, counter
+    counter += 1
+    return gcd_rec(b, a % b, counter)
 
 
 if __name__ == '__main__':
     main()
-
