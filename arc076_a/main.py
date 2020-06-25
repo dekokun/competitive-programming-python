2b0bc903ddeb385273bddddd8c69d2cadad7a181
dekokun <dekokun@hatena.ne.jp>
2020-06-25 13:59:57 +0900

    arc076_a pass

--- arc076_a/main.py
+++ arc076_a/main.py
@@ -1,4 +1,5 @@
 import sys
+import math
 input = sys.stdin.readline
 
 
@@ -7,8 +8,15 @@ def log(*args):
 
 
 def main():
+    n, m = map(int, input().rstrip().split())
+    if abs(n - m) >= 2:
+        print("0")
+        return
+    if n == m:
+        print(math.factorial(n) * math.factorial(m) * 2 % (pow(10, 9) + 7))
+    else:
+        print(math.factorial(n) * math.factorial(m) % (pow(10, 9) + 7))
 
 
 if __name__ == '__main__':
     main()
-
