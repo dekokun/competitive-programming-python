3e0a34cb22cb7273abfa40bcd79e498e2034ad19
dekokun <dekokun@hatena.ne.jp>
2020-06-26 08:32:27 +0900

    arc059_a pass

--- arc059_a/main.py
+++ arc059_a/main.py
@@ -7,8 +7,16 @@ def log(*args):
 
 
 def main():
+    ans = 1000000000
+    _n = int(input())
+    a = list(map(int, input().rstrip().split()))
+    for i in range(-100, 101):
+        tmp = 0
+        for v in a:
+            tmp += pow((v - i), 2)
+        ans = min(tmp, ans)
+    print(ans)
 
 
 if __name__ == '__main__':
     main()
-
