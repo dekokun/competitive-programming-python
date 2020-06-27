738413bf1c8cd8f67c2f6042ff90eba9b6aab806
dekokun <dekokun@hatena.ne.jp>
2020-06-27 22:17:27 +0900

    abc172_c pass

--- abc172_c/main.py
+++ abc172_c/main.py
@@ -7,8 +7,39 @@ def log(*args):
 
 
 def main():
+    _n, _m, k = map(int, input().split())
+    ns = list(map(int, input().rstrip().split()))
+    ms = list(map(int, input().rstrip().split()))
+    sum = 0
+    count = 0
+    for v in ns:
+        if sum + v > k:
+            break
+        else:
+            sum += v
+            count += 1
+
+    ans_now = count
+    ans = ans_now
+    i = count - 1
+    for v in ms:
+        if sum + v > k:
+            while sum + v > k:
+                if i < 0:
+                    break
+                sum -= ns[i]
+                i -= 1
+                ans_now -= 1
+            if sum + v > k:
+                break
+            sum += v
+            ans_now += 1
+        else:
+            sum += v
+            ans_now += 1
+        ans = max(ans, ans_now)
+    print(ans)
 
 
 if __name__ == '__main__':
     main()
-
