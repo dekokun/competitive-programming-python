205e96be4e062e4ed18d3cb35723ac967786c36e
dekokun <dekokun@hatena.ne.jp>
2020-06-30 08:32:12 +0900

    arc035_b pass

--- arc035_b/main.py
+++ arc035_b/main.py
@@ -1,4 +1,6 @@
 import sys
+from collections import Counter
+import math
 input = sys.stdin.readline
 
 
@@ -7,8 +9,23 @@ def log(*args):
 
 
 def main():
+    n = int(input())
+    mod = 10 ** 9 + 7
+    l = [int(input()) for _ in range(n)]
+    c = Counter(l)
+    sumTime = 0
+    penalty = 0
+    for v in sorted(l):
+        sumTime += v
+        penalty += sumTime
+    combi = 1
+    for v in c:
+        count = c[v]
+        combi *= math.factorial(count)
+        combi %= mod
+    print(penalty)
+    print(combi)
 
 
 if __name__ == '__main__':
     main()
-
