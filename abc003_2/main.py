4ecc04dcd8bdc963e945117bbc4f423f04cdc425
dekokun <dekokun@hatena.ne.jp>
2020-07-03 09:53:06 +0900

    abc003_2 pass

--- abc003_2/main.py
+++ abc003_2/main.py
@@ -7,8 +7,18 @@ def log(*args):
 
 
 def main():
+    s = input().rstrip()
+    t = input().rstrip()
+    for i in range(len(t)):
+        if s[i] == t[i]:
+            continue
+        if s[i] == '@' and t[i] in 'atcoder' or t[i] == '@' and s[i] in 'atcoder':
+            continue
+        else:
+            print('You will lose')
+            return
+    print('You can win')
 
 
 if __name__ == '__main__':
     main()
-
