936af7fa44c575764446c9e681e725d84187636f
dekokun <dekokun@hatena.ne.jp>
2020-07-05 21:44:37 +0900

    abc173_d pass

--- abc173_d/main.py
+++ abc173_d/main.py
@@ -1,4 +1,5 @@
 import sys
+import collections
 input = sys.stdin.readline
 
 
@@ -7,8 +8,24 @@ def log(*args):
 
 
 def main():
+    n = int(input())
+    a = map(int, input().rstrip().split())
+    ans = 0
+    candidates = collections.deque()
+    for v in sorted(a, reverse=True):
+        if not candidates:
+            candidates.appendleft(v)
+            continue
+        if candidates[-1] > v:
+            v2 = candidates.pop()
+            ans += v2
+            candidates.appendleft(v)
+            candidates.appendleft(v)
+        else:
+            candidates.appendleft(v)
+            ans += v
+    print(ans)
 
 
 if __name__ == '__main__':
     main()
-
