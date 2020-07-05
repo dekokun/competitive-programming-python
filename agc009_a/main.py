f668f7725d7ec0e9bd6474131a6266e4bc95fc2a
dekokun <dekokun@hatena.ne.jp>
2020-07-05 09:16:34 +0900

    agc009_a pass

--- agc009_a/main.py
+++ agc009_a/main.py
@@ -7,8 +7,23 @@ def log(*args):
 
 
 def main():
+    n = int(input())
+    al = []
+    bl = []
+    for _ in range(n):
+        a, b = map(int, input().split())
+        al.append(a)
+        bl.append(b)
+    ans = 0
+    for (i, a) in reversed(list(enumerate(al))):
+        b = bl[i]
+        a += ans
+        a = a % b
+        if a == 0:
+            continue
+        ans += b - a
+    print(ans)
 
 
 if __name__ == '__main__':
     main()
-
