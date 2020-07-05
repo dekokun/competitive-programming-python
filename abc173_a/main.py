7ca18d045fb3f4ae4c5520ce5ed901f5e3685357
dekokun <dekokun@hatena.ne.jp>
2020-07-05 21:46:12 +0900

    abc173_a pass

--- abc173_a/main.py
+++ abc173_a/main.py
@@ -7,8 +7,12 @@ def log(*args):
 
 
 def main():
+    n = int(input())
+    if n % 1000 == 0:
+        print(0)
+    else:
+        print(1000 - n % 1000)
 
 
 if __name__ == '__main__':
     main()
-
