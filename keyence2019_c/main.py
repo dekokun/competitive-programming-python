516c07bfd1a3d2f502718131e2bdbd07adf9a7cb
dekokun <dekokun@hatena.ne.jp>
2020-07-06 20:53:57 +0900

    keyence2019_c pass

--- keyence2019_c/main.py
+++ keyence2019_c/main.py
@@ -7,8 +7,29 @@ def log(*args):
 
 
 def main():
+    n = int(input())
+    a = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
+    minus_sum = 0
+    pluses = []
+    ans = 0
+    for i in range(n):
+        if a[i] < b[i]:
+            minus_sum += b[i] - a[i]
+            ans += 1
+        else:
+            pluses.append(a[i] - b[i])
+    pluses.sort(reverse=True)
+    for v in pluses:
+        if minus_sum <= 0:
+            break
+        minus_sum -= v
+        ans += 1
+    if minus_sum > 0:
+        print(-1)
+    else:
+        print(ans)
 
 
 if __name__ == '__main__':
     main()
-
