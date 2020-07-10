f1dbc0d70ced231a040df00b7950b3ea33c56cae
dekokun <dekokun@hatena.ne.jp>
2020-07-10 13:58:42 +0900

    agc015_b pass

--- agc015_b/main.py
+++ agc015_b/main.py
@@ -7,8 +7,17 @@ def log(*args):
 
 
 def main():
+    s = input().rstrip()
+    ans = 0
+    for (i, c) in enumerate(s):
+        if c == 'U':
+            ans += len(s) - i - 1
+            ans += 2 * i
+        else:
+            ans += (len(s) - i - 1) * 2
+            ans += i
+    print(ans)
 
 
 if __name__ == '__main__':
     main()
-
