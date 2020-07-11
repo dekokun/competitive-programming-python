68f10382f495dbc8d8c9a7a38b817f239226ff39
dekokun <dekokun@hatena.ne.jp>
2020-07-11 16:41:36 +0900

    abc042_b pass

--- abc042_b/main.py
+++ abc042_b/main.py
@@ -7,8 +7,10 @@ def log(*args):
 
 
 def main():
+    N, _L = map(int, input().rstrip().split())
+    s = [input().rstrip() for _ in range(N)]
+    print(''.join(sorted(s)))
 
 
 if __name__ == '__main__':
     main()
-
