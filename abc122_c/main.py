5c7905b4cf9e94ea81c2f31763c7b6a08866a97a
dekokun <dekokun@hatena.ne.jp>
2020-07-15 08:38:18 +0900

    abc122_c pass

--- abc122_c/main.py
+++ abc122_c/main.py
@@ -7,8 +7,24 @@ def log(*args):
 
 
 def main():
+    n, q = map(int, input().split())
+    s = input().rstrip()
+    before = ''
+    start_cumsum = []
+    now_val = 0
+    for (i, c) in enumerate(s):
+        if c == 'A':
+            before = 'A'
+        elif c == 'C' and before == 'A':
+            now_val += 1
+            before = ''
+        else:
+            before = ''
+        start_cumsum.append(now_val)
+    for _ in range(q):
+        l, r = map(int, input().split())
+        print(start_cumsum[r - 1] - start_cumsum[l - 1])
 
 
 if __name__ == '__main__':
     main()
-
