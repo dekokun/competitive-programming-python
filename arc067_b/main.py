7112272e6f5b9702c0ea0edde9c11ab45d0585fb
dekokun <dekokun@hatena.ne.jp>
2020-07-16 20:41:07 +0900

    arc067_b pass

--- arc067_b/main.py
+++ arc067_b/main.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 input = sys.stdin.readline
 
 
@@ -7,8 +8,16 @@ def log(*args):
 
 
 def main():
+    n, a, b = map(int, input().rstrip().split())
+    x = list(map(int, input().rstrip().split()))
+    ans = 0
+    for diff in np.diff(x):
+        if diff * a < b:
+            ans += diff * a
+        else:
+            ans += b
+    print(ans)
 
 
 if __name__ == '__main__':
     main()
-
