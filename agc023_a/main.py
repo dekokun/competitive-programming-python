86df9a145d4cf15e5646a7e0d03c30f51ac0e06a
dekokun <dekokun@hatena.ne.jp>
2020-07-17 08:02:24 +0900

    agc023_a pass

--- agc023_a/main.py
+++ agc023_a/main.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 import numpy as np
 input = sys.stdin.readline
 
@@ -9,6 +10,17 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    n = int(input())
+    a = [*map(int, input().rstrip().split())]
+    m = defaultdict(int)
+    for v in np.append(np.cumsum(a), 0):
+        m[v] += 1
+    ans = 0
+    for key in m:
+        v = m[key]
+        if v > 1:
+            ans += v * (v - 1) // 2
+    print(ans)
 
 
 if __name__ == '__main__':
