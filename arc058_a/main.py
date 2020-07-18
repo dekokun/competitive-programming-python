ccf49235be19a374b5bf1a336eb8dad97f17baf9
dekokun <dekokun@hatena.ne.jp>
2020-07-18 12:52:33 +0900

    arc058_a pass

--- arc058_a/main.py
+++ arc058_a/main.py
@@ -9,6 +9,18 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    n, k = map(int, input().rstrip().split())
+    ds = set(map(int, input().rstrip().split()))
+    for i in range(n, 1000000):
+        continue_flag = False
+        for c in str(i):
+            if int(c) in ds:
+                continue_flag = True
+        if continue_flag:
+            continue
+        else:
+            print(i)
+            return
 
 
 if __name__ == '__main__':
