acbce879d30ba563880a6399e934c53a43c8e088
dekokun <dekokun@hatena.ne.jp>
2020-07-20 13:15:48 +0900

    abc169_b pass

--- abc169_b/main.py
+++ abc169_b/main.py
@@ -9,6 +9,16 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    N = int(input())
+    A = list(map(int, input().rstrip().split()))
+    A.sort()
+    ans = 1
+    for v in A:
+        ans *= v
+        if ans > pow(10, 18):
+            ans = -1
+            break
+    print(ans)
 
 
 if __name__ == '__main__':
