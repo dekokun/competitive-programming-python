b4be476322d8425b5ad4a34c9e1139af56f41190
dekokun <dekokun@hatena.ne.jp>
2020-07-21 21:01:32 +0900

    tmp commit

--- abc169_d/main.py
+++ abc169_d/main.py
@@ -1,5 +1,7 @@
 import sys
+import math
 import numpy as np
+from collections import defaultdict
 input = sys.stdin.readline
 
 
@@ -9,6 +11,27 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    N = int(input())
+    d = defaultdict(int)
+    for i in range(2, math.ceil(math.sqrt(N))):
+        while N % i == 0:
+            d[i] += 1
+            N //= i
+    if not N == 1:
+        d[N] += 1
+
+    ans = 0
+    for key in d:
+        val = d[key]
+        tmp = 0
+        count = 0
+        for i in range(1, val):
+            tmp += i
+            count += 1
+            if tmp >= val:
+                break
+        ans += max(count, 1)
+    print(ans)
 
 
 if __name__ == '__main__':
