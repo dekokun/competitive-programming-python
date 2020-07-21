d4486f9d997f08e0ece15fcc0367187941c5ef3c
dekokun <dekokun@hatena.ne.jp>
2020-07-21 21:08:02 +0900

    arc057_a pass

--- arc057_a/main.py
+++ arc057_a/main.py
@@ -9,6 +9,16 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    A, K = map(int, input().rstrip().split())
+    if K == 0:
+        print(2 * pow(10, 12) - A)
+        return
+    now = A
+    day = 0
+    while now < 2 * pow(10, 12):
+        day += 1
+        now += 1 + K * now
+    print(day)
 
 
 if __name__ == '__main__':
