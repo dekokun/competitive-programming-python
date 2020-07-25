853dbc152c39d076ae6392b07e18132bcb168613
dekokun <dekokun@hatena.ne.jp>
2020-07-25 21:14:24 +0900

    m_solutions2020_a pass

--- m_solutions2020_a/main.py
+++ m_solutions2020_a/main.py
@@ -9,6 +9,24 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    X = int(input())
+    if X < 600:
+        ans = 8
+    elif X < 800:
+        ans = 7
+    elif X < 1000:
+        ans = 6
+    elif X < 1200:
+        ans = 5
+    elif X < 1400:
+        ans = 4
+    elif X < 1600:
+        ans = 3
+    elif X < 1800:
+        ans = 2
+    elif X < 2000:
+        ans = 1
+    print(ans)
 
 
 if __name__ == '__main__':
