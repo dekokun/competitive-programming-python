ac09aaefd93874d8e483ea35488aeb40eba724e6
dekokun <dekokun@hatena.ne.jp>
2020-07-25 21:18:37 +0900

    m_solutions2020_c initialize

---
+++ m_solutions2020_c/main.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+input = sys.stdin.readline
+
+
+def log(*args):
+    print(*args, file=sys.stderr)
+
+
+def main():
+    np.set_printoptions(threshold=20)
+
+
+if __name__ == '__main__':
+    main()
