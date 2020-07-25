9d5c37d989f7ba2bbaea98b7cc7fd5e0f2008c4b
dekokun <dekokun@hatena.ne.jp>
2020-07-25 21:18:20 +0900

    m_solutions2020_b pass

--- m_solutions2020_b/main.py
+++ m_solutions2020_b/main.py
@@ -9,6 +9,19 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    A, B, C = map(int, input().rstrip().split())
+    K = int(input())
+    # A < B < C
+    while A >= B:
+        B *= 2
+        K -= 1
+    while B >= C:
+        C *= 2
+        K -= 1
+    if K >= 0:
+        print("Yes")
+    else:
+        print("No")
 
 
 if __name__ == '__main__':
