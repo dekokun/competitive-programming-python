45c6519f815477a2d8225128fdc01a11478a78a3
dekokun <dekokun@hatena.ne.jp>
2020-07-27 13:56:10 +0900

    arc100_a pass

--- arc100_a/main.py
+++ arc100_a/main.py
@@ -9,6 +9,26 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    N = int(input())
+    l = list(map(int, input().rstrip().split()))
+    for i, v in enumerate(l):
+        l[i] = v - (i + 1)
+    l.sort()
+    now = 0
+    for v in l:
+        now += abs(v - (l[0] - 1))
+    right_count = N
+    left_count = 0
+    ans = now
+    before = l[0] - 1
+    for v in l:
+        now -= right_count * (v - before)
+        now += left_count * (v - before)
+        right_count -= 1
+        left_count += 1
+        ans = min(now, ans)
+        before = v
+    print(ans)
 
 
 if __name__ == '__main__':
