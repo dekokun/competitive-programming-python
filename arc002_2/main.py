db6e277cf0c5a998af02cd062bc447608b09a718
dekokun <dekokun@hatena.ne.jp>
2020-07-28 20:24:41 +0900

    arc002_2 pass

--- arc002_2/main.py
+++ arc002_2/main.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+from datetime import datetime
+from datetime import timedelta
 input = sys.stdin.readline
 
 
@@ -9,6 +11,13 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    S = input().rstrip()
+    date = datetime.strptime(S, "%Y/%m/%d")
+    for i in range(0, 1000000000):
+        added = date + timedelta(i)
+        if added.year % (added.day * added.month) == 0:
+            print(added.strftime("%Y/%m/%d"))
+            return
 
 
 if __name__ == '__main__':
