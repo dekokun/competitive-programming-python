42ca2a50db63fc9dbc781574e26f59bc8dc8235d
dekokun <dekokun@hatena.ne.jp>
2020-08-01 16:29:07 +0900

    arc030_1 pass

--- arc030_1/main.py
+++ arc030_1/main.py
@@ -1,14 +1,14 @@
 import sys
-import numpy as np
 input = sys.stdin.readline
 
 
-def log(*args):
-    print(*args, file=sys.stderr)
-
-
 def main():
-    np.set_printoptions(threshold=20)
+    n = int(input())
+    k = int(input())
+    if n - k >= k:
+        print("YES")
+    else:
+        print("NO")
 
 
 if __name__ == '__main__':
