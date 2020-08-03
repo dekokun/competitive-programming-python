36ad008d2e8be65bd8cc19dde7ea5883febdaf0b
dekokun <dekokun@hatena.ne.jp>
2020-08-03 13:42:21 +0900

    abc174_c pass

--- abc174_c/main.py
+++ abc174_c/main.py
@@ -9,6 +9,19 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    K = int(input())
+    if K % 2 == 0 or K % 5 == 0:
+        print(-1)
+        return
+    cnt = 0
+    now = 7
+    while True:
+        cnt += 1
+        if now % K == 0:
+            print(cnt)
+            return
+        now = now * 10 + 7
+        now %= K
 
 
 if __name__ == '__main__':
