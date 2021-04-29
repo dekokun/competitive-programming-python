9d133b5c57b3c9d0e2e99fb130764f934ef24aa5
dekokun <dekokun@hatena.ne.jp>
2021-04-29 21:49:00 +0900

    typical90_b pass

--- typical90_b/main.py
+++ typical90_b/main.py
@@ -9,6 +9,25 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    n = int(input())
+    if n % 2 == 1:
+        print("")
+        return
+    for s in dfs("", [], n):
+        print(s)
+
+
+def dfs(first, stack, length):
+    if len(first) + len(stack) >= length:
+        stack.reverse()
+        return [first + "".join(stack)]
+    stack1 = stack.copy()
+    stack1.append(')')
+    a = dfs(first + '(', stack1, length)
+    if not len(stack) == 0:
+        next = stack.pop()
+        a = a + dfs(first + next, stack, length)
+    return a
 
 
 if __name__ == '__main__':
