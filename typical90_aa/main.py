185e38fc040d9590e0234817270d6a53311ec9fa
dekokun <dekokun@hatena.ne.jp>
2021-04-29 21:29:00 +0900

    typical90_aa pass

--- typical90_aa/main.py
+++ typical90_aa/main.py
@@ -9,6 +9,13 @@ def log(*args):
 
 def main():
     np.set_printoptions(threshold=20)
+    n = int(input())
+    se = set()
+    for i in range(1, n + 1):
+        s = input()
+        if not s in se:
+            print(i)
+        se.add(s)
 
 
 if __name__ == '__main__':
